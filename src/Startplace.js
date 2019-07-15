79e28ac2787d7d528e7dd0fdceef72605fab851c
Olga Melnikova <shelga.melnikova@gmail.com>
2019-07-15 22:42:11 +0300

    wip

---
+++ src/Startplace.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Startplace.css';
+
+function Startplace() {
+    return (
+        <div className="startplace">
+        <div classNane="search">Bookz</div>
+        <input className="search" type="text" placeholder="Search books by name, genre and ect ..."></input>
+        
+        </div>
+    );
+}
+
+export default Startplace
