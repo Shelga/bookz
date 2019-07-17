3625dfdd1b3d2afe567a559e811bbad0b2209e48
Olga Melnikova <shelga.melnikova@gmail.com>
2019-07-17 23:39:31 +0300

    menu

--- src/MenuTop.js
+++ src/MenuTop.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import './MenuTop.css';
+import img2 from './static/menu.svg'
 import img3 from './static/search.svg'
-import img4 from './static/heart.svg'
+import img4 from './static/whiteheart.svg'
+import img5 from './static/menu1.svg'
+import img6 from './static/user.svg'
+import img7 from './static/star.svg'
 
-function MenuTop () {
+function MenuTop() {
     return (
-<div className="menutop">
-    <div>
-        <h3>DISCOVER</h3>
-        <div className="menuOne">
-            <div className="text2">
-                <img className="img2"></img>
-                <p className="text">Home</p>
+        <div className="menutop">
+            <div>
+                <h3 className="menuOne">DISCOVER</h3>
+                <div>
+                    <div className="text2">
+                        <img className="img2" src={img2}></img>
+                        <div className="text">Home</div>
+                    </div>
+                    <div className="text2">
+                        <img className="img2" src={img3}></img>
+                        <div className="text">Browse</div>
+                    </div>
+                    <div className="text2 borderForYou">
+                        <img className="img2" src={img4}></img>
+                        <div className="text">For You</div>
+                    </div>
+                </div>
             </div>
-            <img className="img3" src={img3}></img>
-            <p className="text">Browse</p>
-            <img className="img4" src={img4}></img>
-            <p className="text">For You</p>
+            <div>
+                <h3 className="menuOne">LIBRARY</h3>
+                <div className="text2">
+                    <img className="img2" src={img5}></img>
+                    <div className="text">Playlists</div>
+                </div>
+                <div className="text2">
+                    <img className="img2" src={img6}></img>
+                    <div className="text">Authors</div>
+                </div>
+                <div className="text2">
+                    <img className="img2" src={img7}></img>
+                    <div className="text">Favourites</div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div>
-        <h3>LIBRARY</h3>
-        <img className="img5"></img>
-        <p className="text">Plailists</p>
-        <img className="img6"></img>
-        <p className="text">Authors</p>
-        <img className="img7"></img>
-        <p className="text">Favourites</p>
-    </div>
-</div>
 
     );
 }
