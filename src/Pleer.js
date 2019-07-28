e11e8f6cf79fa00faca05bd1a89e06df986bfc6f
Olga Melnikova <shelga.melnikova@gmail.com>
2019-07-28 23:41:05 +0300

    pleer

--- src/Pleer.js
+++ src/Pleer.js
@@ -3,7 +3,8 @@ import './Pleer.css';
 import { ReactComponent as Next } from './static/next.svg'
 import { ReactComponent as Pause } from './static/pause.svg'
 import { ReactComponent as Previous } from './static/previous.svg'
-
+import { ReactComponent as PauseButton } from './static/pauseButton.svg'
+import { ReactComponent as PlayButton } from './static/playButton.svg'
 
 function Pleer() {
     return (
@@ -17,15 +18,40 @@ function Pleer() {
                     </div>
                 </div>
                 <div className="pleerMenu">
-                    <div>5:50</div>
+                    <div className="timePast">5:50</div>
                     <div className="booksPlayer">
                         <Previous className="pleerIcon" />
                         <Pause className="pleerIcon" />
                         <Next className="pleerIcon" />
                     </div>
-                    <div>5:25</div>
+                    <div className="timeFuture">5:25</div>
                 </div>
                 <div className="pleerBlack">
+                    <div className="chapter">
+                        <PlayButton className="blackPleerIcon"/>
+                        <div>Chapter 01</div>
+                        <div></div>
+                    </div>
+                    <div className="chapter">
+                        <PauseButton className="blackPleerIcon" />
+                        <div>Chapter 02</div>
+                        <div></div>
+                    </div>
+                    <div className="chapter">
+                        <PlayButton className="blackPleerIcon" />
+                        <div>Chapter 03</div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <PlayButton className="blackPleerIcon" />
+                        <div>Chapter 04</div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <PlayButton className="blackPleerIcon" />
+                        <div>Chapter 05</div>
+                        <div></div>
+                    </div>
                 </div>
 
             </div>
